13b9b55f69acead5d207fcd447b76f03a78d604f
Nikhil139-codes <kushwahanikhil431@gmail.com>
2026-03-29 23:27:11 +0530

    Added deployment config

--- app.py
+++ app.py
@@ -29,4 +29,6 @@ def ask():
 # RUN
 # -----------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render dynamic port deta hai
+    app.run(host="0.0.0.0", port=port)
